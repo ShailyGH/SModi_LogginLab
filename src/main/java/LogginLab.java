a9e90ffac6a07db4f6b31b698db1ad4f4d212b11
ShailyGH <shailykunal@.com>
2024-03-03 14:45:59 -0500

    Finished

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -1,34 +1,42 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class LogginLab {
     private final static Logger logger = Logger.getLogger(LogginLab.class.getName());
 
     private Integer threshold = 0;
+   // private Integer thresholdReached = 0;
 
-    public LogginLab() {
+    public LogginLab()
+    {
         this.threshold = 0;
     }
 
-    public static void main(String[] args) {
-
+    // public LogginLab()
+    // {
+    //      this.thresholdReached = 0;
+   // }
+    public static void main(String[] args)
+    {
         logger.log(Level.INFO, "Hello World!");
         logger.log(Level.SEVERE, "Terrible Error!");
         logger.log(Level.WARNING, "Not So Bad Error!");
-
         logger.log(Level.INFO, "****\n\tAt ZipCode, \n\twe don't use System.out.Println \n\tuntil we've earned the right.\n****");
-
     }
 
-    public Integer getThreshold() {
+    public Integer getThreshold()
+    {
         return threshold;
     }
 
-    public void setThreshold(Integer threshold) {
+    public void setThreshold(Integer threshold)
+    {
         this.threshold = threshold;
     }
 
-    public boolean thresholdExceeds(Integer limit) {
+    public boolean thresholdExceeds(Integer limit)
+    {
         if (this.threshold > limit) 
           return true;
         else
@@ -38,4 +46,11 @@ public class LogginLab {
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
+    public boolean thresholdReached(Integer limit)
+    {
+        if (this.threshold < limit)
+            return true;
+        else
+            return false;
+    }
 }
